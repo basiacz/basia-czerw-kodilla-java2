2fb2c5108df1a6a32367c1bf0418a34921d3755a
basiacz <basiaczerw@gmail.com>
2018-03-10 16:49:09 +0100

    15.2

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -16,7 +16,7 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(long id){
-        return repository.findById(id);
+    public Task getTaskById(final Long id){
+        return repository.findById(id).orElse(null);
     }
 }
