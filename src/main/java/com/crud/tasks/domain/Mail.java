0bf64e4ef20bb54e394bd61e0c74dee6e35f0a47
basiacz <basiaczerw@gmail.com>
2018-04-22 19:56:56 +0200

    19.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCc;
+
+    public Mail(final String mailTo, final String subject, final String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+    }
+}
