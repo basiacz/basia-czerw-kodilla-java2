4d6841ff13b0a5db22711b72c6f67c28cba95af0
basiacz <basiaczerw@gmail.com>
2018-04-30 22:02:14 +0200

    21.2

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -2,10 +2,12 @@ package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public interface TaskRepository extends CrudRepository <Task, Long> {
     @Override
     List<Task> findAll();
